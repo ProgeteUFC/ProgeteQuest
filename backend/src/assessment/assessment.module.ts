778c47b64b1f1fe6307423d76e5aac0163f06ff6
Vanderlei <derlei.vc@gmail.com>
2025-07-01 22:25:45 -0300

    feat: Implementa endpoints GET e POST para Assessment

--- backend/src/assessment/assessment.module.ts
+++ backend/src/assessment/assessment.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AssessmentController } from './assessment.controller';
 import { AssessmentService } from './assessment.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Assessment } from './entities/assessment.entity';
+import { Class } from 'src/class/entities/class.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Assessment, Class])],
   controllers: [AssessmentController],
-  providers: [AssessmentService]
+  providers: [AssessmentService],
 })
 export class AssessmentModule {}
