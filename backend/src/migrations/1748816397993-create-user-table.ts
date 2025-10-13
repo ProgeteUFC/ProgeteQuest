d7cdd1e577cc7f54a42bdf14c3af5012a6fe0b6b
JoseVilmar <jose.vilmar.brito.sousa@gmail.com>
2025-10-12 23:31:34 -0300

    Update 1748816397993-create-user-table.ts
    Adicionado coluna type na migration user para criação automática da mesma.

--- backend/src/migrations/1748816397993-create-user-table.ts
+++ backend/src/migrations/1748816397993-create-user-table.ts
@@ -9,6 +9,7 @@ export class CreateUserTable1748816397993 implements MigrationInterface {
         name VARCHAR(50) NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
         password VARCHAR NOT NULL,
+        type VARCHAR(20) NOT NULL,
 
         created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
         updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
