7baf92c62ca9274566cce68143f93479dc32ba28
JoseVilmar <jose.vilmar.brito.sousa@gmail.com>
2026-03-26 19:36:03 -0300

    Migrations para Forum, Topic e Post
    Arquivos responsáveis por criar as tabelas no banco de dados de fórum, tópico e post.

---
+++ backend/src/migrations/1762349750343-create-forum-table.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateForumTable1762349750343 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      CREATE TABLE public.forum (
+        forum_id VARCHAR(36) NOT NULL,
+        turma_id VARCHAR(36) NOT NULL,
+        created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+        updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW() NOT NULL,
+        PRIMARY KEY (forum_id),
+        FOREIGN KEY (turma_id) REFERENCES public.class(class_id) ON DELETE CASCADE ON UPDATE CASCADE
+      );
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP TABLE public.forum;');
+  }
+}
