6a45326458d36439543fa2eb9e6e02ba736f3347
EdgarFurts02 <josenildodooeste@gmail.com>
2026-04-02 18:05:29 -0300

    planeta, logo e formulário adicionado

--- frontend/src/pages/CriarTurma/index.tsx
+++ frontend/src/pages/CriarTurma/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import Header from "../../components/Header";
-import { Container, PageWrapper } from "./styles";
+import logo from "../../assets/progete.png";
+import PlanetOrange from "../../assets/planet_orange.png";
+import { Container, PageWrapper, FormCard, CreateButton, ButtonWrapper, Logo, Planet } from "./styles";
+
 
 
 export default function CriarTurma() {
@@ -13,15 +16,27 @@ export default function CriarTurma() {
   }, []);
   return (
     <>
+        
       <Header />
-
       <PageWrapper>
         <Container>
           <h1>Criar turma</h1>
           <h2>
-            Aqui você pode criar uma turma para a realização das atividades com os alunos
+            Aqui você pode criar uma turma para a realização das <br />
+             atividades com os alunos
           </h2>
+          <FormCard>
+          <input type="text" placeholder="Nome da turma " />
+  <textarea placeholder="Descrição (opcional)" />
+        </FormCard>
+
+        <ButtonWrapper>
+        <CreateButton>CRIAR</CreateButton>
+        </ButtonWrapper>
         </Container>
+        <Planet src= {PlanetOrange} alt="planeta laranja" />
+        <Logo src={logo} alt="logo do projeto" />
+   
       </PageWrapper>
     </>
   );
--- frontend/src/pages/CriarTurma/styles.ts
+++ frontend/src/pages/CriarTurma/styles.ts
@@ -7,10 +7,28 @@ export const PageWrapper = styled.div`
 
   display: flex;
   flex-direction: column;
-  justify-content: center; /* central vertical */
-  align-items: center;     /* central horizontal */
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding-left: 9rem;
+  position:relative;
 
-  padding: 2rem;
+  padding-top: 2rem;
+`;
+
+export const Planet = styled.img`
+position: fixed;
+  width: 1500px;
+  bottom: -100px;
+  right: -80px;
+  transform: translate(20%, 20%);
+`;
+
+export const Logo = styled.img`
+  width: 180px;
+  position: absolute;
+  bottom: 130px;
+  left: 2rem;
+
 `;
 
 export const Container = styled.div`
@@ -18,7 +36,86 @@ font-size: 24px;
   font-weight: 800;
   color: ${({ theme }) => theme.colors.white};
   font-family: "Baloo Paaji 2", sans-serif;
+  
 
   margin: 0;
   padding-top: 2rem;
+`;
+
+export const FormCard = styled.div`
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 1rem;
+  border-radius: 20px;
+  font-family: "Baloo Paaji 2", sans-serif;
+
+  width: 100%;
+  max-width: 500px;
+
+  margin-top: 2rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  input {
+    border: none;
+    border-radius: 999px;
+    padding: 0.6rem 1rem;
+
+    background-color: ${({ theme }) => theme.colors.indigo};
+    color: white;
+
+    font-size: 0.9rem;
+  }
+
+  textarea {
+    border: none;
+    border-radius: 16px;
+    padding: 1rem;
+
+    background-color: ${({ theme }) => theme.colors.indigo};
+    color: white;
+
+    min-height: 100px;
+    resize: none;
+  }
+
+  input::placeholder,
+  textarea::placeholder {
+    color: white;
+    opacity: 0.8;
+  }
+
+  
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  max-width: 500px;
+
+  display: flex;
+  justify-content: flex-end;
+
+  margin-top: 1rem;
+`;
+
+export const CreateButton = styled.button`
+  margin-top: 1rem;
+  font-family: "Baloo Paaji 2", sans-serif;
+  width: 200px;
+  font-size: 15px;
+  height: 35px;
+  margin-left: auto;
+
+  align-self: flex-end;
+
+  padding: 0.3rem 1.2rem;
+  border-radius: 999px;
+
+  border: none;
+  background-color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.primaryDark};
+
+  font-weight: bold;
+  cursor: pointer;
 `;
