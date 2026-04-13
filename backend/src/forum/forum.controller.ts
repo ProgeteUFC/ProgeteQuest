421dc93880a96ad2f1b99c370419690dfc55dfb8
JoseVilmar <jose.vilmar.brito.sousa@gmail.com>
2026-04-12 21:53:45 -0300

    Enhance forum controller with user context and pagination
    No controller foi adicionado roles e o uso do payloader para se adequar com o restante do projeto, progredindo assim com os endpoints.

--- backend/src/forum/forum.controller.ts
+++ backend/src/forum/forum.controller.ts
@@ -6,9 +6,16 @@ import {
   Param,
   UsePipes,
   ValidationPipe,
+  Query,
 } from '@nestjs/common';
 import { ForumService } from './forum.service';
 import { CreateTopicDto, CreatePostDto } from './dtos/index';
+import {
+  TopicListResponseDto,
+  PostListResponseDto,
+} from './dtos/responses/index';
+import { Roles } from 'src/decorators/roles.decorator';
+import { User, UserPayload } from 'src/decorators/user.decorator';
 
 @Controller()
 @UsePipes(new ValidationPipe({ transform: true, whitelist: true }))
@@ -20,29 +27,43 @@ export class ForumController {
     return this.forumService.createForum(turmaId);
   }
 
+  @Roles('teacher', 'student')
   @Post('forum/:forumId/topic')
   async createTopic(
     @Param('forumId') forumId: string,
     @Body() createTopicDto: CreateTopicDto,
+    @User() user: UserPayload,
   ) {
-    return this.forumService.createTopic(forumId, createTopicDto);
+    return this.forumService.createTopic(forumId, createTopicDto, user.userId);
   }
 
+  @Roles('teacher', 'student')
   @Get('forum/:forumId/topics')
-  async getTopics(@Param('forumId') forumId: string) {
-    return this.forumService.listTopicsByForum(forumId);
+  async getTopics(
+    @Param('forumId') forumId: string,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+  ): Promise<TopicListResponseDto> {
+    return this.forumService.listTopicsByForum(forumId, page, limit);
   }
 
+  @Roles('teacher', 'student')
   @Get('topic/:topicId/posts')
-  async getPosts(@Param('topicId') topicId: string) {
-    return this.forumService.listPostsByTopic(topicId);
+  async getPosts(
+    @Param('topicId') topicId: string,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 15,
+  ): Promise<PostListResponseDto> {
+    return this.forumService.listPostsByTopic(topicId, page, limit);
   }
 
+  @Roles('teacher', 'student')
   @Post('topic/:topicId/post')
   async createPost(
     @Param('topicId') topicId: string,
     @Body() createPostDto: CreatePostDto,
+    @User() user: UserPayload,
   ) {
-    return this.forumService.createPost(topicId, createPostDto);
+    return this.forumService.createPost(topicId, createPostDto, user.userId);
   }
 }
