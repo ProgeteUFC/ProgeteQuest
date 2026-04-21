32538af0564010a888ad2875a67fdda508fe7a93
leticiapuga <leticiademirandapuga@gmail.com>
2026-04-21 17:14:23 -0300

    fix: replace react-helmet with react-helmet-async

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import { HelmetProvider } from 'react-helmet-async';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <HelmetProvider>
       <App />
+    </HelmetProvider>
       
     </BrowserRouter>
   </StrictMode>
