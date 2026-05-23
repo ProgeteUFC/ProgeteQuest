32cdafdf5a8dc7ff12750c410b2ca8f310adecdf
Rian Lucas <144740653+RianLMAsl@users.noreply.github.com>
2026-05-23 11:05:50 -0300

    Revert "Implementação da tela de Login Professor e algumas alterações no app.tsx pra conseguir fazer o processamento da tela"
    This reverts commit 28fdde4a9379d37c3e54907f57b2b988a9adb2c4.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LoginProfessor from './LoginProfessor'
 
 function App() {
   const [count, setCount] = useState(0)
