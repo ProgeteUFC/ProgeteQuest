81fa47fa1e48490e584644aae74546d66538f69e
leticiapuga <leticiademirandapuga@gmail.com>
2026-06-30 11:02:31 -0300

    fix: add definite assignment assertions to activity entity

--- backend/src/activity/entities/activity.entity.ts
+++ backend/src/activity/entities/activity.entity.ts
@@ -21,63 +21,63 @@ export class Activity {
     length: 36,
     name: 'activity_id',
   })
-  activityId: string;
+  activityId!: string;
 
   @Column({
     type: 'varchar',
     length: 50,
     nullable: false,
   })
-  name: string;
+  name!: string;
 
   @Column({
     type: 'timestamp',
     nullable: false,
   })
-  date: Date;
+  date!: Date;
 
   @Column({
     type: 'enum',
     enum: ActivityType,
     nullable: false,
   })
-  type: ActivityType;
+  type!: ActivityType;
 
   @Column({
     type: 'varchar',
     length: 36,
     name: 'class_id',
   })
-  classId: string;
+  classId!: string;
 
   @Column({
     type: 'varchar',
     length: 36,
     name: 'assessment_id',
   })
-  assessmentId: string;
+  assessmentId!: string;
 
   @CreateDateColumn({ name: 'created_at' })
-  createdAt: Date;
+  createdAt!: Date;
 
   @UpdateDateColumn({ name: 'updated_at' })
-  updatedAt: Date;
+  updatedAt!: Date;
 
   // Relação com Class (1,1)
   @ManyToOne(() => Class, (cls) => cls.activities)
   @JoinColumn({ name: 'class_id' })
-  class: Class;
+  class!: Class;
 
   // Relação com Assessment (1,1)
   @ManyToOne(() => Assessment, (assessment) => assessment.activities)
   @JoinColumn({ name: 'assessment_id' })
-  assessment: Assessment;
+  assessment!: Assessment;
 
   //Relação com Code (0,n)
   @OneToMany(() => Code, (code) => code.activity)
-  codes: Code[];
+  codes!: Code[];
 
   // Relação com Checkin (0,n)
   @OneToMany(() => Checkin, (checkin) => checkin.activity)
-  checkins: Checkin[];
+  checkins!: Checkin[];
 }
