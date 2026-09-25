b3df6e13e33378a10c49952059a11e5d6f37b8b2
JoseVilmar <jose.vilmar.brito.sousa@gmail.com>
2026-09-25 20:46:48 -0300

    Modify migration to add active column if not exists
    Alteração feita para garantir que não quebre caso já exista a coluna

--- backend/src/migrations/1790298359845-add_active_to_user.ts
+++ backend/src/migrations/1790298359845-add_active_to_user.ts
@@ -4,7 +4,7 @@ export class AddActiveToUser1790298359845 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       ALTER TABLE public.user
-      ADD COLUMN active BOOLEAN NOT NULL DEFAULT true;
+      ADD COLUMN IF NOT EXISTS active BOOLEAN NOT NULL DEFAULT true;
     `);
   }
 
