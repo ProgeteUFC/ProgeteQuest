dfc066daeadd036eaa4f38d47043e3971a751884
JoseVilmar <jose.vilmar.brito.sousa@gmail.com>
2026-09-25 19:31:45 -0300

    Nova coluna em user
    coluna que permite validar se o usuário está ativo

---
+++ backend/src/migrations/1790298359845-add_active_to_user.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AddActiveToUser1790298359845 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE public.user
+      ADD COLUMN active BOOLEAN NOT NULL DEFAULT true;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      ALTER TABLE public.user
+      DROP COLUMN IF EXISTS active;
+    `);
+  }
+}
